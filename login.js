2ac0cae314c97aff943314bd399a780a506db8a4
Shimaa-Saber <shimaasaber812@gmail.com>
2024-12-13 16:27:39 +0200

    handle some thing

---
+++ login.js
@@ -0,0 +1,99 @@
+let nameInput = document.getElementById('name');
+let emailInput = document.getElementById('email');
+let passInput = document.getElementById('pass');
+let conPassInput = document.getElementById('conpass');
+let checkInput = document.getElementById('check');
+
+
+let nameError = document.getElementById('sname');
+let emailError = document.getElementById('semail');
+let passError = document.getElementById('spass');
+let conPassError = document.getElementById('scpass');
+let checkError = document.getElementById('scheck');
+
+
+function validateName() {
+    if (nameInput.value.length <3) {
+     
+     nameError.style.display = "block";  
+      return false;  
+    } else {
+         nameError.textContent = "";  
+        nameError.style.display = "none";  
+      return true;   
+    }
+  }
+  
+  function validateEmail() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  
+    if (!emailRegex.test(emailInput.value)) {
+      
+        emailError.style.display = "block";
+      return false;
+    } else {
+        emailError.textContent = "";
+        emailError.style.display = "none";
+      return true;
+    }
+  }
+  
+  function validatePassword() {
+    if (passInput.value.length < 8) {
+      
+        passError.style.display = "block";
+      return false;
+    } else {
+        passError.textContent = "";
+        passError.style.display = "none";
+      return true;
+    }
+  }
+
+
+  function validateConPass(){
+    if(conPassInput.value!=passInput.value){
+        conPassError.style.display="block" ;
+        return false;
+    }else{
+        conPassError.textContent="";
+        conPassError.style.display="none";
+        return true;
+    }
+  }
+
+
+  
+
+nameInput.oninput = validateName;
+emailInput.oninput = validateEmail;
+passInput.oninput = validatePassword;
+conPassInput.oninput=validateConPass;
+function validate(){
+  if(!isNaN(nameInput)|| nameInput.trim()===""){
+   
+    nameError.textContent ="the name must be char only" ;
+    nameError.style.display = "block";  
+    
+    }
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  
+    if(!re.test(emailInput)){
+      
+      
+    
+          emailError.style.display = "block";   
+      
+      }
+
+      if (passInput.value.length < 8) {
+      
+        passError.style.display = "block";
+      }
+
+      if(conPassInput.value!=passInput.value){
+        conPassError.style.display="block" ;
+      }
+
+
+
+
+}
