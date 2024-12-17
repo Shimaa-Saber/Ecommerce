94c1b5ea36cd314b49188e5609e0389a01a5e87c
Shimaa-Saber <shimaasaber812@gmail.com>
2024-12-17 02:09:21 +0200

    handle some errors in product detailes and inhance the structure of the code

---
+++ JS/productDetailes.js
@@ -0,0 +1,17 @@
+
+let counter=document.getElementById("count");
+
+function decreaseQuantity() {
+    if (counter.value > 0) {
+        counter.value--;
+     
+    }
+  }
+
+  function increaseQuantity() {
+   
+        counter.value++;
+     
+    
+  }
+
