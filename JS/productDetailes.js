b187f2f02fc35999a7b2271b9cafc01e73378c86
Shimaa-Saber <shimaasaber812@gmail.com>
2024-12-26 17:34:53 +0200

    handle responsive peoduct detailes page

--- JS/productDetailes.js
+++ JS/productDetailes.js
@@ -33,34 +33,26 @@ function decreaseQuantity() {
       .then((product) => {
         const detailsContainer = document.getElementById("product-details-container");
         detailsContainer.innerHTML = `
-          <img src="${product.image}" alt=""  id="img1">
-  <div id="div1">
-   <h3>${product.title}</h3>
-   <h3 style="margin-top: 15px; color: #FF2020">${product.price}</h3>
-   <span style="display: block;">Category :  <span style="margin-left: 20px; color:#FF2020;">${product.category}</span></span>
-  
-   <span id="span2" style="padding-top:15px ;margin:10px" >Availibility :  <span  id="span3" style="margin-left: 20px; ">in Stock</span></span>
-   
-   <hr style="margin-top:70px">
-   <p>
-      ${product.description}
-   </p>
-
-   <div id="div2">
-       <div id="div3">
-        
-       <button id="mins" style=" border:none;background-color:transparent;font-size:25px;margin-right:5px" onclick="decreaseQuantity()">-</button>
-       <input id="count" type="number" min="0"value="${product.quantity}" style=" margin-top:5px; margin-left:5px;border:none; width:37px;text-align:center;font-size:20px">
-       <button id="plus"  style=" border: none;background-color:transparent;font-size:25px" onclick="increaseQuantity()">+</button>
-       </div>
-       
-   </div>
-
-   <button id="add_to_cart" onclick="addToCart(${product.id})">ADD TO CART</button>
-  
-  
-  </div>
-        `;
+        <div class="product-container">
+          <img src="${product.image}" alt="Product Image" id="img1">
+          <div id="div1">
+            <h3>${product.title}</h3>
+            <h3 style="margin-top: 15px; color: #FF2020">${product.price}</h3>
+            <span style="display: block;">Category : <span style="margin-left: 20px; color:#FF2020;">${product.category}</span></span>
+            <span id="span2" style="padding-top:15px ;margin:10px">Availability: <span id="span3" style="margin-left: 20px;">In Stock</span></span>
+            <hr style="margin-top:70px">
+            <p>${product.description}</p>
+            <div id="div2">
+              <div id="div3">
+                <button id="mins" style="border: none; background-color: transparent; font-size: 25px; margin-right: 5px;" onclick="decreaseQuantity()">-</button>
+                <input id="count" type="number" min="0" value="${product.quantity}" style="margin-top: 5px; margin-left: 5px; border: none; width: 37px; text-align: center; font-size: 20px">
+                <button id="plus" style="border: none; background-color: transparent; font-size: 25px;" onclick="increaseQuantity()">+</button>
+              </div>
+            </div>
+            <button id="add_to_cart" onclick="addToCart(${product.id})">ADD TO CART</button>
+          </div>
+        </div>
+      `;
       })
       .catch((error) => console.error("Error fetching product details:", error));
   }
