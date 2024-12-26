4f597b924ebc607b7e8a04df246d98db8361c6a8
Shimaa-Saber <shimaasaber812@gmail.com>
2024-12-26 16:38:13 +0200

    handle login logic and make it responsive + make slider and window shipped with Bootstrap

--- JS/home.js
+++ JS/home.js
@@ -12,40 +12,40 @@ const all=document.getElementById("all");
 
 
 
-let sliderImg = 0;
-let img = document.getElementById("slider-img");
-let slider = document.getElementsByTagName("header");
+// let sliderImg = 0;
+// let img = document.getElementById("slider-img");
+// let slider = document.getElementsByTagName("header");
 
 
 
-var interval;
+// var interval;
 
-interval = setInterval(next, 1500);
-
-
- slider[0].addEventListener('mouseenter',()=>clearInterval(interval));   
- nextbtn.addEventListener('mouseenter',()=>clearInterval(interval));
- prevbtn.addEventListener('mouseenter',()=>clearInterval(interval));
- slider[0].addEventListener('mouseleave',()=>interval = setInterval(next, 1500));
- function next() {
-    sliderImg++;
-    sliderImg = ((sliderImg % 3) + 3) % 3;
-    img.setAttribute("src", `images/${sliderImg}.jpg`);
-}
-function prev(){
-
-    sliderImg--;
-    sliderImg = ((sliderImg % 3) + 3) % 3;
-    img.setAttribute("src", `images/${sliderImg}.jpg`);
-}
+// interval = setInterval(next, 1500);
 
 
+//  slider[0].addEventListener('mouseenter',()=>clearInterval(interval));   
+//  nextbtn.addEventListener('mouseenter',()=>clearInterval(interval));
+//  prevbtn.addEventListener('mouseenter',()=>clearInterval(interval));
+//  slider[0].addEventListener('mouseleave',()=>interval = setInterval(next, 1500));
+//  function next() {
+//     sliderImg++;
+//     sliderImg = ((sliderImg % 3) + 3) % 3;
+//     img.setAttribute("src", `images/${sliderImg}.jpg`);
+// }
+// function prev(){
+
+//     sliderImg--;
+//     sliderImg = ((sliderImg % 3) + 3) % 3;
+//     img.setAttribute("src", `images/${sliderImg}.jpg`);
+// }
 
 
-nextbtn.addEventListener('click', next);
 
 
-prevbtn.addEventListener('click',prev);
+// nextbtn.addEventListener('click', next);
+
+
+// prevbtn.addEventListener('click',prev);
 
 
 function fetchProducts(category = "") {
